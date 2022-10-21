d49801848d979a26feb6048482a60707ce06c527
Yi-Te Huang <y.t.d.huang@phys.ncku.edu.tw>
2022-10-21 16:15:26 +0800

    Change parallel computing (distributed) to multi-threading

--- src/heom_matrices/M_Boson.jl
+++ src/heom_matrices/M_Boson.jl
@@ -65,82 +65,77 @@ function M_Boson(
     nvec2idx = hierarchy.nvec2idx
 
     # start to construct the matrix
-    L_row = distribute([Int[] for _ in procs()])
-    L_col = distribute([Int[] for _ in procs()])
-    L_val = distribute([ComplexF64[] for _ in procs()])
-    channel = RemoteChannel(() -> Channel{Bool}(), 1) # for updating the progress bar
-
+    L_row = Int[]
+    L_col = Int[]
+    L_val = ComplexF64[]
+    lk = SpinLock()
     if verbose
-        println("Preparing block matrices for HEOM liouvillian superoperator (using $(nprocs()) processors)...")
+        println("Preparing block matrices for HEOM liouvillian superoperator (using $(nthreads()) threads)...")
         flush(stdout)
         prog = Progress(Nado; desc="Processing: ", PROGBAR_OPTIONS...)
     end
-    @sync begin # start two tasks which will be synced in the very end
-        # the first task updates the progress bar
-        @async while take!(channel)
-            if verbose
-                next!(prog)
-            else
-                put!(channel, false) # this tells the printing task to finish
-            end
+    @threads for idx in 1:Nado
+        nvec = idx2nvec[idx]
+        n_exc = sum(nvec)
+        if n_exc >= 1
+            sum_ω = bath_sum_ω(nvec, baths)
+            op = Lsys - sum_ω * I_sup
+        else
+            op = Lsys
+        end
+        lock(lk)
+        try
+            add_operator!(op, L_row, L_col, L_val, Nado, idx, idx)
+        finally
+            unlock(lk)
         end
 
-        # the second task does the computation
-        @async begin
-            @distributed (+) for idx in 1:Nado
-                nvec = idx2nvec[idx]
-                n_exc = sum(nvec)
-                if n_exc >= 1
-                    sum_ω = bath_sum_ω(nvec, baths)
-                    op = Lsys - sum_ω * I_sup
-                else
-                    op = Lsys
-                end
-                add_operator!(op, L_row, L_col, L_val, Nado, idx, idx)
-
-                count = 0
-                nvec_neigh = copy(nvec)
-                for bB in baths
-                    for k in 1:bB.Nterm
-                        count += 1
-                        n_k = nvec[count]
-                        if n_k >= 1
-                            nvec_neigh[count] = n_k - 1
-                            idx_neigh = nvec2idx[nvec_neigh]
-                            
-                            op = prev_grad_boson(bB, k, n_k)
-                            add_operator!(op, L_row, L_col, L_val, Nado, idx, idx_neigh)
-
-                            nvec_neigh[count] = n_k
-                        end
-                        if n_exc <= tier - 1
-                            nvec_neigh[count] = n_k + 1
-                            idx_neigh = nvec2idx[nvec_neigh]
-                            
-                            op = next_grad_boson(bB)
-                            add_operator!(op, L_row, L_col, L_val, Nado, idx, idx_neigh)
-                            
-                            nvec_neigh[count] = n_k
-                        end
+        count = 0
+        nvec_neigh = copy(nvec)
+        for bB in baths
+            for k in 1:bB.Nterm
+                count += 1
+                n_k = nvec[count]
+                if n_k >= 1
+                    nvec_neigh[count] = n_k - 1
+                    idx_neigh = nvec2idx[nvec_neigh]
+                    
+                    op = prev_grad_boson(bB, k, n_k)
+                    lock(lk)
+                    try
+                        add_operator!(op, L_row, L_col, L_val, Nado, idx, idx_neigh)
+                    finally
+                        unlock(lk)
                     end
+                    nvec_neigh[count] = n_k
                 end
-                if verbose
-                    put!(channel, true) # trigger a progress bar update
+                if n_exc <= tier - 1
+                    nvec_neigh[count] = n_k + 1
+                    idx_neigh = nvec2idx[nvec_neigh]
+                    
+                    op = next_grad_boson(bB)
+                    lock(lk)
+                    try
+                        add_operator!(op, L_row, L_col, L_val, Nado, idx, idx_neigh)
+                    finally
+                        unlock(lk)
+                    end
+                    nvec_neigh[count] = n_k
                 end
-                1 # Here, returning some number 1 and reducing it somehow (+) is necessary to make the distribution happen.
             end
-            put!(channel, false) # this tells the printing task to finish
+        end
+        if verbose
+            next!(prog)
         end
     end
     if verbose
         print("Constructing matrix...")
         flush(stdout)
     end
-    L_he = sparse(vcat(L_row...), vcat(L_col...), vcat(L_val...), Nado * sup_dim, Nado * sup_dim)
+    L_he = sparse(L_row, L_col, L_val, Nado * sup_dim, Nado * sup_dim)
     if verbose
         println("[DONE]")
         flush(stdout)
     end
-    d_closeall()  # release all distributed arrays created from the calling process
     return M_Boson(L_he, tier, Nsys, Nado, Nado, 0, sup_dim, :none, Bath, hierarchy)
 end
--- src/heom_matrices/heom_matrix_base.jl
+++ src/heom_matrices/heom_matrix_base.jl
@@ -216,9 +216,9 @@ end
 
 function add_operator!(op, I, J, V, N_he, row_idx, col_idx)
     row, col, val = pad_coo(op, N_he, N_he, row_idx, col_idx)
-    append!(localpart(I)[1], row)
-    append!(localpart(J)[1], col)
-    append!(localpart(V)[1], val)
+    append!(I, row)
+    append!(J, col)
+    append!(V, val)
 end
 
 # sum ω of bath for current gradient
