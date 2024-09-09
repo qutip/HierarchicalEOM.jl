ef0c2f33e7efdb2cf825b4893dc9a0b67428693f
Yi-Te Huang <y.t.d.huang@phys.ncku.edu.tw>
2024-09-09 11:25:35 +0800

    fix doc

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,7 @@ using Documenter, HierarchicalEOM
 
 DocMeta.setdocmeta!(HierarchicalEOM, :DocTestSetup, :(using HierarchicalEOM); recursive = true)
 
-const DRAFT = true # set `true` to disable cell evaluation
+const DRAFT = false # set `true` to disable cell evaluation
 
 ENV["GKSwstype"] = "100" # enable headless mode for GR to suppress warnings when plotting
 
@@ -32,22 +32,6 @@ mkdir(doc_output_path)
 # Generate page: Quick Start
 QS_source_file = abspath(joinpath(@__DIR__, "..", "examples", "quick_start.jl"))
 Literate.markdown(QS_source_file, doc_output_path)
-
-# Generate example pages
-EXAMPLES = ["cavityQED", "dynamical_decoupling", "SIAM", "electronic_current"]
-EX_source_files = [abspath(joinpath(@__DIR__, "..", "examples", "$(ex_name).jl")) for ex_name in EXAMPLES]
-EX_output_files = ["examples/$(ex_name).md" for ex_name in EXAMPLES]
-for file in EX_source_files
-    Literate.markdown(file, doc_output_path)
-end
-
-# Generate benchmark pages
-BENCHMARKS = ["benchmark_ODE_solvers", "benchmark_LS_solvers"]
-BM_source_files = [abspath(joinpath(@__DIR__, "..", "examples", "$(bm_name).jl")) for bm_name in BENCHMARKS]
-BM_output_files = ["examples/$(bm_name).md" for bm_name in BENCHMARKS]
-for file in BM_source_files
-    Literate.markdown(file, doc_output_path)
-end
 
 const PAGES = Any[
     "Home"=>Any[
@@ -80,8 +64,6 @@ const PAGES = Any[
         "Time Evolution"=>"time_evolution.md",
         "Stationary State"=>"stationary_state.md",
         "Spectrum"=>"spectrum.md",
-        "Examples"=>EX_output_files,
-        "Benchmark Solvers"=>BM_output_files,
         "Extensions"=>Any["CUDA.jl"=>"extensions/CUDA.md"],
     ],
     "Library API"=>"libraryAPI.md",
