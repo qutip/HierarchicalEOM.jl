3f26cfbfdcaaad40d6b661a596d2a2b9460de49b
cailixun <cailixun23@gmail.com>
2025-12-16 11:46:15 +0800

    format

--- src/heom_matrices/heom_matrix_base.jl
+++ src/heom_matrices/heom_matrix_base.jl
@@ -202,7 +202,10 @@ function Base.:(-)(M::AbstractHEOMLSMatrix, Sup::HEOMSuperOp)
     return _reset_HEOMLS_data(M, M.data - Sup)
 end
 
-SciMLOperators.cache_operator(M::AbstractHEOMLSMatrix{T}, cachevec::AbstractVector) where {T<:SciMLOperators.AddedOperator} = _reset_HEOMLS_data(M, cache_operator(M.data, cachevec))
+SciMLOperators.cache_operator(
+    M::AbstractHEOMLSMatrix{T},
+    cachevec::AbstractVector,
+) where {T<:SciMLOperators.AddedOperator} = _reset_HEOMLS_data(M, cache_operator(M.data, cachevec))
 
 @doc raw"""
     SciMLOperators.iscached(M::AbstractHEOMLSMatrix)
