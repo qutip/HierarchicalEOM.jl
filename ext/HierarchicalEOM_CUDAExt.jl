0447b4836e60b8dc63bf435163fe4a39072c36f1
cailixun <cailixun23@gmail.com>
2026-04-14 19:25:14 +0800

    [skip-ci] format

--- ext/HierarchicalEOM_CUDAExt.jl
+++ ext/HierarchicalEOM_CUDAExt.jl
@@ -66,7 +66,7 @@ CuSparseMatrixCSR{T}(M::HEOMSuperOp) where {T} =
 
 _convert_to_gpu_matrix(A::AbstractSparseMatrix, MType::Type{T}) where {T <: AbstractCuSparseMatrix} = MType(A)
 _convert_to_gpu_matrix(A::AbstractMatrix, MType::Type{T}) where {T <: AbstractCuSparseMatrix} = MType(sparse(A))
-_convert_to_gpu_matrix(A::Diagonal{Etype}, ::Type{T}) where {T<:(CUDA.CUSPARSE.AbstractCuSparseMatrix), Etype<:Number} = Diagonal(CuArray{Etype, 1}(A.diag))
+_convert_to_gpu_matrix(A::Diagonal{Etype}, ::Type{T}) where {T <: AbstractCuSparseMatrix, Etype <: Number} = Diagonal(CuArray{Etype, 1}(A.diag))
 
 
 _convert_to_gpu_matrix(A::MatrixOperator, MType) = MatrixOperator(_convert_to_gpu_matrix(A.A, MType))
