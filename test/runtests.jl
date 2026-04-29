71abbc9a8a71db7f55502cdb10208961dd735d3e
Yi-Te Huang <yitehuang.tw@gmail.com>
2026-04-29 18:50:02 +0800

    fix typo

--- test/runtests.jl
+++ test/runtests.jl
@@ -41,6 +41,7 @@ if (GROUP == "CUDA_Ext") # || (GROUP == "All")
     Pkg.instantiate()
 
     using HierarchicalEOM
+    using SparseArrays
     using LinearSolve
     using CUDA
     CUDA.allowscalar(false) # Avoid unexpected scalar indexing
