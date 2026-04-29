0c84bdfa051a45976c21d13cb77e4a0d60be4f6f
Yi-Te Huang <44385685+ytdHuang@users.noreply.github.com>
2026-04-29 12:54:44 +0800

    Add warning message for non-sparse operators and `BosonBath` generating method (#277)

--- test/M_S.jl
+++ test/M_S.jl
@@ -14,6 +14,7 @@
     @test L.data == L_super.data
     @test isconstant(L)
     @test iscached(L)
+    @test issparse(L.data.A) # check if it's a sparse matrix
     ados_list = heomsolve(L, ψ0, 0:1:t; reltol = 1.0e-8, abstol = 1.0e-10, progress_bar = Val(false)).ados
     ados = ados_list[end]
     @test ados.dims == L.dims
